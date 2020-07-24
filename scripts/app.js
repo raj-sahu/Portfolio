c89b81c41ba3a429a01f63920f281cfaaa50e5d0
raj-sahu <be10363.18@bitmesra.ac.in>
2020-07-24 16:49:01 +0530

    footer updated

--- scripts/app.js
+++ scripts/app.js
@@ -1,36 +1,26 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
-
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
-});
-*/
-/* Otherwise just put the config content (json): */
 var typed = new Typed("#Hello", {
-  strings: ["Hi ,", "I am Raj Sahu"],
+  strings: ["Hi ,", "I am Raj Sahu "],
   typeSpeed: 100,
+  backSpeed: 110,
   smartBackspace: true,
   startDelay: 2,
   showCursor: false,
+  loop: true,
 });
-
 particlesJS(
   "particle",
 
   {
     particles: {
       number: {
-        value: 100,
+        value: 120,
         density: {
           enable: true,
-          value_area: 1000,
+          value_area: 800,
         },
       },
       color: {
-        value: "#9AECDB",
+        value: "#596275",
       },
       shape: {
         type: "circle",
@@ -70,7 +60,7 @@ particlesJS(
       line_linked: {
         enable: true,
         distance: 150,
-        color: "#ffffff",
+        color: "#596275",
         opacity: 0.4,
         width: 1,
       },
@@ -129,7 +119,127 @@ particlesJS(
     retina_detect: true,
     config_demo: {
       hide_card: false,
-      background_color: "#b61924",
+      background_color: "#e15f41",
+      background_image: "",
+      background_position: "50% 50%",
+      background_repeat: "no-repeat",
+      background_size: "cover",
+    },
+  }
+);
+particlesJS(
+  "particle1",
+
+  {
+    particles: {
+      number: {
+        value: 120,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: "#596275",
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000",
+        },
+        polygon: {
+          nb_sides: 5,
+        },
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100,
+        },
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false,
+        },
+      },
+      size: {
+        value: 5,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#596275",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 6,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse",
+        },
+        onclick: {
+          enable: true,
+          mode: "push",
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1,
+          },
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3,
+        },
+        repulse: {
+          distance: 200,
+        },
+        push: {
+          particles_nb: 4,
+        },
+        remove: {
+          particles_nb: 2,
+        },
+      },
+    },
+    retina_detect: true,
+    config_demo: {
+      hide_card: false,
+      background_color: "#e15f41",
       background_image: "",
       background_position: "50% 50%",
       background_repeat: "no-repeat",
